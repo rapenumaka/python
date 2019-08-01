95ddb0f605da4d7515a2d17b38271c928c57e82a
rapenumaka <44779218+rapenumaka@users.noreply.github.com>
2019-08-01 14:49:48 -0500

    Update Mappings.py

--- hive_script/Mappings.py
+++ hive_script/Mappings.py
@@ -1,5 +1,5 @@
 from elasticsearch import helpers, Elasticsearch
-es = Elasticsearch(['http://macquarie-dev-es-data-0.dev.digitalreasoning.com:9200'],timeout=600)
+es = Elasticsearch(['some-elastic-url'],timeout=600)
 
 
 mymapping={"mappings": {"geotest":{"properties":{"location":{"type":"geo_point","lat_lon":"True","geohash":"True"}}}}}
